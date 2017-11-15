4d20682782bcc39e26360b41516224c522d335ff
wangjianpeng228@163.com <Hero82726171>
2017-11-15 13:56:17 +0800

    基础部分

--- training-web/src/main/java/com/visionet/hero/provider/HeroStudentProvider.java
+++ training-web/src/main/java/com/visionet/hero/provider/HeroStudentProvider.java
@@ -4,7 +4,7 @@ public class HeroStudentProvider {
 
     public String queryStudentAndRoles(){
 
-        StringBuilder sql = new StringBuilder(" SELECT * ");
+        StringBuilder sql = new StringBuilder(" SELECT s.id_ user_id , s.name_ user_name , r.id_ role_id , r.name_ role_name ");
         sql.append(" FROM hero_student_role sr ")
            .append(" INNER JOIN hero_student s ON sr.student_id = s.id_ ")
            .append(" INNER JOIN hero_role r ON sr.role_id = r.id_ ")
