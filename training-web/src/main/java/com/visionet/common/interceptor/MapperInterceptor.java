4103102e5138fbfcec0aa1eb8e778feb14af4f16
wangjianpeng228@163.com <Hero82726171>
2017-11-15 11:14:04 +0800

    基础部分

---
+++ training-web/src/main/java/com/visionet/common/interceptor/MapperInterceptor.java
@@ -0,0 +1,122 @@
+package com.visionet.common.interceptor;
+
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.ParameterMode;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+})
+public class MapperInterceptor implements Interceptor {
+
+    private Logger logger = LoggerFactory.getLogger("watchSQL");
+
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+    public Object intercept(Invocation invocation) throws Throwable {
+
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+
+        Object parameterObject = null;
+        if (invocation.getArgs().length > 1) {
+            parameterObject = invocation.getArgs()[1];
+        }
+
+        String statementId = mappedStatement.getId();
+        BoundSql boundSql = mappedStatement.getBoundSql(parameterObject);
+        Configuration configuration = mappedStatement.getConfiguration();
+        String sql = getSql(boundSql, parameterObject, configuration);
+
+
+        long start = System.currentTimeMillis();
+
+        long end = System.currentTimeMillis();
+        long timing = end - start;
+
+        logger.debug("********** SQL Start **********");
+        logger.debug("statementId: "+statementId);
+        logger.debug("耗时: " + timing + " ms");
+        logger.debug("SQL: "+sql);
+        logger.debug("********** SQL End **********");
+
+        return invocation.proceed();
+    }
+
+    public Object plugin(Object target) {
+
+        return Plugin.wrap(target, this);
+    }
+
+    public void setProperties(Properties properties) {
+
+    }
+
+    /**
+     * @Description 获取sql语句
+     * @author wangjp
+     * @Date: 2017-11-8 15:31
+     */
+    private String getSql(BoundSql boundSql, Object parameterObject, Configuration configuration) {
+        String sql = boundSql.getSql().replaceAll("[\\s]+", " ");
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+        if (parameterMappings != null) {
+            for (int i = 0; i < parameterMappings.size(); i++) {
+                ParameterMapping parameterMapping = parameterMappings.get(i);
+                if (parameterMapping.getMode() != ParameterMode.OUT) {
+                    Object value;
+                    String propertyName = parameterMapping.getProperty();
+                    if (boundSql.hasAdditionalParameter(propertyName)) {
+                        value = boundSql.getAdditionalParameter(propertyName);
+                    } else if (parameterObject == null) {
+                        value = null;
+                    } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                        value = parameterObject;
+                    } else {
+                        MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                        value = metaObject.getValue(propertyName);
+                    }
+                    sql = replacePlaceholder(sql, value);
+                }
+            }
+        }
+        return sql;
+    }
+
+    private String replacePlaceholder(String sql, Object propertyValue) {
+        String result;
+        if (propertyValue != null) {
+            if (propertyValue instanceof String) {
+                result = "'" + propertyValue + "'";
+            } else if (propertyValue instanceof Date) {
+                result = "'" + DATE_FORMAT.format(propertyValue) + "'";
+            } else {
+                result = propertyValue.toString();
+            }
+        } else {
+            result = "null";
+        }
+        return sql.replaceFirst("\\?", result);
+    }
+
+}
+
